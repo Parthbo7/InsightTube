4d3ec257f3d51c5d4a8ddc50bdf7741840f7df7d
Parthbo7 <parthbulbule123@gmail.com>
2026-03-06 23:14:36 +0530

    trending page added button on Home

--- Home.py
+++ Home.py
@@ -50,7 +50,7 @@ st.markdown("""
 
 
 # -------------------- Action Buttons --------------------
-col1, col2, col3 = st.columns(3)
+col1, col2, col3,col4 = st.columns(4)
 
 with col1:
     if st.button("📊 Channel Analysis", use_container_width=True):
@@ -64,6 +64,9 @@ with col3:
     if st.button("🌍 About Us", use_container_width=True):
         st.switch_page("pages/3_About_Us.py")
 
+with col4:
+    if st.button("🔥 Trending Videos", use_container_width=True):
+        st.switch_page("pages/4_Trending.py")
 st.divider()
 
 
